9740ce4acc75576007bc229e5d2695f86b001e8b
John Serra <john@serra.us>
2023-03-07 20:11:33 -0500

    added test page

---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>This is the test page</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
