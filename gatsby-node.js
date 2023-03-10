0c88a603fc19dd8cec28b9420a0ee6103dd02fbb
John Serra <john@serra.us>
2023-03-09 23:14:30 -0500

    gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,7 @@
-exports.createPages = async ({actions, graphql}) => {
+const path = require("path");
+
+exports.createPages = async ({ actions, graphql }) => {
+    const pageTemplate = path.resolve("src/templates/page.js");
     const { createPage } = actions;
     
     const { data } = await graphql(
@@ -17,8 +20,8 @@ for (let i = 0; i < data.allWpPage.nodes.length; i++){
     const page = data.allWpPage.nodes[i];
     createPage({
         path: page.uri,
-        component:
-    })
+        component: pageTemplate,
+    });
 }
 
 };
